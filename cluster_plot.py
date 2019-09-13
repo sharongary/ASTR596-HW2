f46a6d41ed570f8dc5c3281a5da53f187b860297
Gregory Rudnick <grudnick@ku.edu>
2019-09-13 08:53:59 -0500

    fixed bug

--- cluster_plot.py
+++ cluster_plot.py
@@ -46,13 +46,13 @@ def cmdplot(clustname='', magshift=0,colshift=0):
     plt.plot(refcldat['bp_rp'], refcldat['phot_rp_mean_mag'], 'ro',markersize=3,alpha=0.8, label = 'Pleiades')
     plt.ylim(18, 2)
     plt.xlim(-0.5, 3.5)
-    plt.xlabel('$G_{BP}-G_{RP}$')
-    plt.ylabel('G_{RP}')
+    plt.xlabel('GBP-GRP$')
+    plt.ylabel('GRP')
     plt.title(clustname)
     plt.legend(loc='upper left')
 
-    s = 'Magnitude Shift $=  %.2f$'%(magshift)
-    scol = 'Color Shift $=  %.2f$'%(colshift)
+    s = 'Magnitude Shift =  %.2f'%(magshift)
+    scol = 'Color Shift =  %.2f'%(colshift)
 
     plt.text(1.5, 4,s, horizontalalignment='left',fontsize=20)
     plt.text(1.5, 3,scol, horizontalalignment='left',fontsize=20)
